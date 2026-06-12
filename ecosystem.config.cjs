24a2de51f09bf36d3d1ac74536e4d7a9097473b3
Edward Tanguay (windows) <edward@tanguay.info>
2026-06-12 13:40:00 +0200

    DEV: Reduce to one instance for pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
     {
       name: 'showcase-nuxt-prisma-turso',
       port: '3148',
-      exec_mode: 'cluster',
-      instances: 'max',
+      instances: 1,
       script: './.output/server/index.mjs',
       env: {
         NODE_ENV: 'production',
